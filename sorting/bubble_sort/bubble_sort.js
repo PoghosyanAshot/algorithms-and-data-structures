bb6c013ed879f9cfc23697274407d98cdca59a98
PoghosyanAshot <ashot20032020@gmail.com>
2025-09-16 15:15:53 +0400

    sorting

--- sorting/bubble_sort/bubble_sort.js
+++ sorting/bubble_sort/bubble_sort.js
@@ -2,10 +2,17 @@ function bubble_sort(arr) {
     const size = arr.length;
 
     for (let i = 0; i < size; ++i) {
+        const is_sorted = true;
+        
         for (let j = 1; j < size - i; ++j) {
             if (arr[j] < arr[j - 1]) {
                 [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+                is_sorted = false;
             }
+        }
+
+        if (is_sorted) {
+            break;
         }
     }
 
